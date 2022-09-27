27b6d717cad89554e6f78f1ed55de9a89214ce3d
nedozrel <dixi.starodubtsev@gmail.com>
2022-09-27 22:35:25 +0700

    get_user_friends API view was created

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Q, Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from app.models import Note, Profile, TelegramPhoto
@@ -28,3 +28,19 @@ def get_notes(request, creator_tg_id):
 	note_creator = Profile.objects.get(telegram_id=creator_tg_id)
 	serializer = ProfileNotesSerializer(note_creator)
 	return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_user_friends(request, tg_user_id):
+	friends = Profile.objects.filter(
+		received_notes__creator__telegram_id=tg_user_id
+	).annotate(
+		notes_received=Count('telegram_id')
+	).order_by(
+		'username'
+	).values(
+		'telegram_id',
+		'username',
+		'notes_received'
+	)
+	return Response(friends)
